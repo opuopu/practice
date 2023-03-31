208695b701094d9e68534223c788da63d4d3b388
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-31 19:06:43 +0600

    1 file modified

--- index.js
+++ index.js
@@ -4,25 +4,4 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-const products = [
-  { name: 'opu', price: 1000 },
-  { name: 'akhi', price: 5000 },
-  { name: 'siyam', price: 15000 },
-];
-console.log(products[0].price);
-
-// second work
-
-const friends = ['opu', 'siyam', 'tuhin', 'kelababa', 'mdsgfhnbjksdgfnbkjdfbg'];
-
-const bigFriends = (friends) => {
-  let big = '';
-  for (let i = 0; i < friends.length; i++) {
-    let name = friends[i];
-   big?.length < name.length = big=name
-  }
-  console.log(big);
-  return big;
-};
-
-bigFriends(friends);
+console.log('hello');
