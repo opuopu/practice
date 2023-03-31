ac024b63eaf21824564350514ff535d8237569b8
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-31 19:08:34 +0600

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starterssss</h1>`;
+appDiv.innerHTML = `<h1>JS Startersssss</h1>`;
